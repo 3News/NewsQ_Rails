5ea71be49dde5d02c53bf2e25bccd07a4a1b2491
Hokun Baek <baekhokun@Hokun-ui-iMac.local>
2013-10-04 13:20:54 +0900

    Project 생성

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsQSpike::Application.config.secret_key_base = 'd0c9fbd748a4777f9b0b4b66cbdaf5d3bc4d40ccd4f0808bbc961f24208e0dd609a31d168975f9598070b7e2723783c48aa356e6ab838ff6b87c2fff76d63b0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewsQSpike::Application.config.session_store :cookie_store, key: '_NewsQ_Spike_session'
