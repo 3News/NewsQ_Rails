40a62e80338c7eeb2471b10dbd45e54cdd0744f6
Hokun Baek <baekhokun@Hokun-ui-iMac.local>
2013-10-04 15:08:18 +0900

    annotate 실행

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: feeds
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  url        :string(255)
+#  category   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Feed < ActiveRecord::Base
 end
