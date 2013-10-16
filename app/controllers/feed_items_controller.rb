c8d0c8c3bc8e800a94da4d41e90a52a252d3f806
Hokun Baek <baekhokun@Hokun-ui-iMac.local>
2013-10-16 18:43:11 +0900

    will_paginate 적용

--- app/controllers/feed_items_controller.rb
+++ app/controllers/feed_items_controller.rb
@@ -1,5 +1,7 @@
 class FeedItemsController < ApplicationController
   def index
-    @feed_items = FeedItem.order('published_at DESC')
+    @feed_items = FeedItem.paginate(
+      :page => params[:page], :per_page => 50,
+      :order => 'published_at DESC')
   end
 end
