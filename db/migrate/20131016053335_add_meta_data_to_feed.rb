c60eace48cfa24e2d78009ce77ab953b49976451
Hokun Baek <baekhokun@Hokun-ui-iMac.local>
2013-10-16 14:35:16 +0900

    Feed Model에 etag, last_modified 필드 추가

---
+++ db/migrate/20131016053335_add_meta_data_to_feed.rb
@@ -0,0 +1,6 @@
+class AddMetaDataToFeed < ActiveRecord::Migration
+  def change
+    add_column :feeds, :etag, :string
+    add_column :feeds, :last_modified, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131004075252) do
+ActiveRecord::Schema.define(version: 20131016053335) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20131004075252) do
     t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "etag"
+    t.datetime "last_modified"
   end
 
   create_table "users", force: true do |t|
